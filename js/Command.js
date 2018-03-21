684d054838939d6bec6a520173300f24b8630646
Shakbia Moshiri <shakiba.moshiri@yahoo.com>
2018-03-21 13:58:44 +0330

    Command.js: fix bug in auto-print after using [cd]

--- js/Command.js
+++ js/Command.js
@@ -371,6 +371,7 @@ function Command( screen, path )
             {
                 // now we are not in the home directory and thus we can go back
                 path.pwd.pop();
+                cwd = path.cwd();
                 for( var file in path.root[ cwd ] )
                 {
                     text( file );
@@ -383,6 +384,7 @@ function Command( screen, path )
             if( path.root[ cwd ][ arg ] === 'directory' && arg in path.root[ cwd ] )
             {
                 path.pwd.push( arg );
+                cwd = path.cwd();
                 for( var file in path.root[ cwd ] )
                 {
                     text( file );
